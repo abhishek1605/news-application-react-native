3ddb4d9f61d6073e514124a8d3eaecf4cd3fbf4e
abhishek1605 <abhishekb@digitalremedy.com>
2023-05-09 14:09:52 +0530

    adding the changes to page loader

--- src/components/common/NewsCardLoader/index.tsx
+++ src/components/common/NewsCardLoader/index.tsx
@@ -1,11 +1,11 @@
-import { Center, HStack, Skeleton, Text, VStack } from "native-base";
+import { Center, Divider, HStack, Skeleton, Text, VStack } from "native-base";
 import React from "react";
 
 const NewsCardLoader = () => {
   return (
     <Center w="100%" p="5">
       {[1, 2, 3, 4].map((key) => (
-        <HStack
+        <VStack
           key={key}
           w="100%"
           borderWidth="1"
@@ -13,41 +13,49 @@ const NewsCardLoader = () => {
           rounded="md"
           borderColor="primary.borderColor"
           mb="4"
+          p="3"
         >
           <Skeleton
             flex="1"
-            w="250"
-            h="155"
-            borderBottomLeftRadius="md"
-            borderTopLeftRadius="md"
+            height="200"
+            rounded="lg"
             startColor="primary.secondaryBgColor"
+            mb="2"
           />
-          <VStack p="2" flex="2" space="2">
+          <VStack flex="2" space="2">
             <Skeleton h="6" rounded="full" startColor="primary.buttonColor" />
-            <Skeleton
-              h="3"
-              rounded="full"
-              startColor="primary.secondaryBgColor"
+            <Divider
+              _dark={{ bg: "primary.borderColor" }}
+              _light={{ bg: "primary.borderColor" }}
             />
-            <Skeleton.Text startColor="primary.secondaryBgColor" />
-            <HStack space={2} mt="1">
+            <HStack justifyContent="space-between">
               <Skeleton
                 h="3"
-                w="10"
-                flex="1"
                 rounded="full"
-                startColor="primary.buttonColor"
+                startColor="primary.secondaryBgColor"
+                width="120"
               />
               <Skeleton
                 h="3"
-                w="10"
-                flex="1"
                 rounded="full"
-                startColor="primary.buttonColor"
+                startColor="primary.secondaryBgColor"
+                width="120"
               />
             </HStack>
+            <Divider
+              _dark={{ bg: "primary.borderColor" }}
+              _light={{ bg: "primary.borderColor" }}
+            />
+            <Skeleton.Text startColor="primary.secondaryBgColor" lines={4} />
+            <Skeleton
+              h="3"
+              w="110"
+              flex="1"
+              rounded="full"
+              startColor="primary.buttonColor"
+            />
           </VStack>
-        </HStack>
+        </VStack>
       ))}
     </Center>
   );
